6ec588c84a8da8dc7cb3a4e95dc04c74926d8bb5
rwohlleb <rwohlleb1@gmail.com>
2018-11-17 00:19:04 -0500

    added falling cubes

---
+++ NetworkingGame/Assets/Targets.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targets : MonoBehaviour {
+
+    public float delay = 1f;
+    public GameObject cube;
+    // Use this for initialization
+    void Start() {
+        InvokeRepeating("Spawn", delay, delay);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        Instantiate(cube, new Vector3(Random.Range(-15, 25), 10, 10), Quaternion.identity);
+    }
+}
