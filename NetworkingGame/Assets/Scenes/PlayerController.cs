6a0305790bd82d902cca9aa52504e4f12e5be30e
Garrett <gkute@hotmail.com>
2018-11-21 16:16:24 -0500

    started working on player shooting

---
+++ NetworkingGame/Assets/Scenes/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections; 
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 direction = (Vector2)((worldMousePos - transform.position));
+        direction.Normalize();
+
+        // Create the Bullet from the Bullet Prefab
+        /*var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);*/
+
+        var bullet = (GameObject)Instantiate(
+                                 bulletPrefab,
+                                 transform.position + (Vector3)(direction * 0.5f),
+                                 Quaternion.identity);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = direction * 1000;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
