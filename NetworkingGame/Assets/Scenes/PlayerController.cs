7e19c01b5bcdca133471b7ccfbf7283f8ff28f0a
Garrett <gkute@hotmail.com>
2018-11-22 18:56:09 -0500

    added firing at cursor using ray

--- NetworkingGame/Assets/Scenes/PlayerController.cs
+++ NetworkingGame/Assets/Scenes/PlayerController.cs
@@ -6,37 +6,89 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+    public float speed = 5.0f;
+    private Plane m_ClickPlane;
+
+    RaycastHit hit;
+    private float raycastlenth = 500;
+
+    void Start()
+    {
+        m_ClickPlane = new Plane(-Vector3.forward, Vector3.zero);
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        /*if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Fire();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float dist;
+            if (m_ClickPlane.Raycast(ray, out dist))
+            {
+                Vector3 dir = (ray.GetPoint(dist) - bulletSpawn.position).normalized;
+                GameObject fireball = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+                fireball.GetComponent<Rigidbody>().AddForce(dir * 500, ForceMode.Impulse);
+                Destroy(fireball, 2.0f);
+            }
+        }*/
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * raycastlenth, Color.green); // to see Raycast trajectory from camera chosen
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            Vector3 aimPoint; //point that cursor will mark
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+
+                aimPoint = hit.point;
+
+            }
+            else
+            {                                        //if  ray doesn't hit anything, just make a point 100 units out into ray, to referece
+
+                aimPoint = ray.origin + (ray.direction * 100);//aimPoint is some point 100 unitys into ray line
+
+            }
+
+            GameObject projectile = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation) as GameObject;
+
+            projectile.transform.LookAt(aimPoint);          //fixes when hit point was = (0,0,0);
+
+            Debug.DrawLine(bulletSpawn.transform.position, hit.point, Color.yellow); // to see trajectory of projectile
+
+
+            projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * 250; //this plus the LookAt aimpoint sends a bullet on the correct ray
+
+            Destroy(projectile, 1.0f);
+
         }
     }
 
-    void Fire()
+    void OnTriggerEnter(Collider col)
     {
-        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("Collision Detected");
+        //all projectile colliding game objects should be tagged "Enemy" or whatever in inspector but that tag must be reflected in the below if conditional
+        if (col.gameObject.tag == "Cube")
+        {
+            Destroy(col.gameObject);
+            //add an explosion or something
+            //destroy the projectile that just caused the trigger collision
+            Destroy(gameObject);
+        }
+    }
 
-        Vector2 direction = (Vector2)((worldMousePos - transform.position));
-        direction.Normalize();
-
-        // Create the Bullet from the Bullet Prefab
-        /*var bullet = (GameObject)Instantiate(
-            bulletPrefab,
-            bulletSpawn.position,
-            bulletSpawn.rotation);*/
-
-        var bullet = (GameObject)Instantiate(
-                                 bulletPrefab,
-                                 transform.position + (Vector3)(direction * 0.5f),
-                                 Quaternion.identity);
-
-        // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = direction * 1000;
-
-        // Destroy the bullet after 2 seconds
-        Destroy(bullet, 2.0f);
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collision Detected: OnCollisionEnter");
+        //all projectile colliding game objects should be tagged "Enemy" or whatever in inspector but that tag must be reflected in the below if conditional
+        if (col.gameObject.tag == "Cube")
+        {
+            Destroy(col.gameObject);
+            //add an explosion or something
+            //destroy the projectile that just caused the trigger collision
+            Destroy(gameObject);
+        }
     }
 }
